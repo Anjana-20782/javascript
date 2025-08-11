80334f0b8597260778b91a4438d17742e0ca615a
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-08-11 13:39:49 +0530

    new files added

---
+++ index.js
@@ -0,0 +1,147 @@
+
+// take a string input("red","green","blue")
+
+
+// let color="Green"
+
+// if(color=="red")
+// {
+//     console.log("stop");
+// }
+// else if(color=="yellow")
+// {
+//     console.log("Get ready");
+// }
+// else if(color=="Green")
+// {
+//     console.log("go");
+// }
+// else{
+
+//     console.log("default");
+// }
+
+
+
+
+// take age as input
+
+// let age=70;
+
+// if(age<18)
+// {
+//     console.log("Not eligible to vote")
+// }
+// else if(age>=18 && age<=59)
+// {
+//     console.log("Eligible to vote")
+// }
+// else{
+
+//      console.log("Eligible senior citizen")
+// }
+
+
+
+
+// take 3 sides of a triangle
+
+// var a=10;
+// var b=17;
+// var c=15;
+
+// if(a==b && a==c && b==c)
+// {
+//     console.log("equilateral")
+// }
+// else if(a==b||b==c)
+// {
+//     console.log("isoceles")
+// }
+// else{
+
+//     console.log("scalene")
+// }
+
+
+
+// ask for uesrname and password
+
+// var a="admin";
+// var b=1234;
+
+
+// if(a=="admin" && b==1234)
+// {
+//     console.log("login successfull");
+// }
+// else if(b!=1234)
+// {
+//     console.log("incorrect password");
+// }
+// else
+// {
+//     console.log("incorrect username");
+
+
+
+// if anumber is posive,negative.zero
+
+// var a=0;
+
+// if(a>0)
+// {
+//     console.log("a is positive")
+// }
+// else if(a<0)
+// {
+//     console.log("a is negative")
+// }
+// else
+// {
+//     console.log("a is zero")
+// }
+
+
+
+
+// grades
+
+var marks=65;
+
+// switch(marks>0)
+// switch(true)
+// {
+//     case (marks>=80):console.log("A grade")
+//     break;
+//      case (marks>=60):console.log("B grade")
+//     break;
+//      case (marks>=40):console.log("C grade")
+//     break;
+//      case (marks>=30):console.log("D  grade")
+//     break;
+//      case (marks>=20):console.log("F grade")
+//     break;
+//     default:console.log("Failed")
+//     break;
+// }
+
+
+
+// leap year
+
+// var a=2005;
+
+// if(a%4==0)
+// {
+//     console.log("it is leap year");
+// }
+// else{
+
+//     console.log("not leap yeaar")
+// }
+
+
+
+// 
+
