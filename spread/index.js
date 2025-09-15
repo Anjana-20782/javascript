e97d3d4fbb0b3f43262ef032b698fe9e85c5560a
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-09-15 13:30:35 +0530

    spread,destructing contents added

---
+++ spread/index.js
@@ -0,0 +1,53 @@
+// arr1=["hello",1,true];
+// arr2=["hai",2,false];
+// let arr3=[...arr1,...arr2];
+// console.log(arr3);
+
+
+
+
+// obj1={
+
+//     name:"anjana",
+//     age:22
+// }
+
+// obj2={
+
+//     role:"faculty",
+//     course:"mern"
+// }
+
+// let obj3={...obj1,...obj2}
+// console.log(obj3)
+
+
+
+// let arr=[1,2,3];
+// let [a,b,c]=arr;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// let arr=[1,2,3];
+// let [a, ,b]=arr;
+// console.log(a);
+// console.log(b);
+
+
+// let obj={name1:"anjana",age:22,couse:"mern"}
+// let {name1,age}=obj;
+// console.log(name1);
+// console.log(age);
+
+
+
+// let obj1={name1:"anjana",age:20};
+// let obj2={course:"mern"};
+// let obj3={...obj1,...obj2};
+// let {...others}=obj3;
+// console.log(others.name1);
+// console.log(others.age);
+
+
+console.log(this);
