12874617a20e0a3319f3b5a0b179e42d3b591b3b
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-09-16 13:05:54 +0530

    this new contents added

---
+++ this/index.js
@@ -0,0 +1,56 @@
+// let obj={
+
+//     name1:"anjana",
+//     new:function(){
+
+//         const arrow= ()=>{
+
+//             console.log(this.name1)
+//         }
+//         arrow()
+//     }
+
+// }
+// obj.new()
+
+
+
+
+
+// let obj={
+
+//     name1:"anjana",
+//     new:function(){
+
+//         console.log(this.name1)
+//     }
+// }
+// obj.new()
+
+
+
+// function myName(name,city){
+
+// console.log(`My name is ${name}.i'm live in ${city} and my age ${this.age}`)
+
+// }
+
+// const obj={
+
+//     age:20
+// }
+// myName.call(obj,"Anjana","Palakkad")
+
+
+
+function myName(name,city){
+
+console.log(`My name is ${name}.i'm live in ${city} and my age ${this.age}`)
+
+}
+
+const obj={
+
+    age:20
+}
+myName.apply(obj,["Anjana","Palakkad"])
