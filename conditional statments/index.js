84280c1583f005b02e5f6cb8041e58a847f537ae
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-09-18 12:36:31 +0530

    conditional statmets added

---
+++ conditional statments/index.js
@@ -0,0 +1,80 @@
+// if statement
+
+// let x=10;
+// let y=20;
+
+// console.log("program started")
+// if(x>y){
+
+//     console.log(x)
+// }
+
+// if(x<y){
+
+//     console.log(y)
+// }
+
+// console.log("program finished")
+
+
+
+// if...else statment
+
+
+// let x=10;
+// let y=20;
+
+// console.log("program started")
+// if(x>y){
+
+//     console.log(x)
+// }
+
+// else{
+
+//     console.log(y)
+// }
+
+// console.log("program finished")
+
+
+// if...else..if
+
+// let x="3"
+
+// if(x=="1"){
+
+// console.log("monday")
+// }
+// else if(x=="2"){
+
+// console.log("tuesday")
+// }
+// if(x=="3"){
+
+// console.log("wednesday")
+// }
+
+// else
+// {
+//     console.log("defalut value")
+// }
+
+
+// switch statments
+
+let x=3
+
+switch(x){
+
+    case 1:console.log("monday")
+    break;
+    case 2:console.log("tueday")
+    break;
+    case 3:console.log("wednesday")
+    break;
+    case 4:console.log("thrusday")
+    break;
+    default:console.log("invalid")
+    break;
+}
